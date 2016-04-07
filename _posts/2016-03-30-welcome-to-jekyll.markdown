733a11adf7d4bc9ddfd402a7b6b2551f2c19fa5c
pollobravo <bravo@graficos.net>
2016-04-07 10:56:13 +0200

    added things to gulp. Edited typos. Changed styles name

--- _posts/2016-03-30-welcome-to-jekyll.markdown
+++ _posts/2016-03-30-welcome-to-jekyll.markdown
@@ -1,8 +1,8 @@
 ---
 layout: post
-title:  "Yellowky"
+title:  "Yellowsky"
 date:   2016-03-30 00:06:52 +0200
-categories: design development ui ux 
+categories: design development ui ux
 ---
 
 You’ll find this post in your `_posts` directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run `jekyll serve`, which launches a web server and auto-regenerates your site when a file is updated.
--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,106 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync');
-var sass        = require('gulp-sass');
-var prefix      = require('gulp-autoprefixer');
-var cp          = require('child_process');
+
+/**
+* Gulp tasks
+*/
+
+var gulp            = require('gulp'),
+    browserSync     = require('browser-sync'),
+    path            = require('path'),
+    gutil           = require('gulp-util'),
+    sass            = require('gulp-sass'),
+    prefix          = require('gulp-autoprefixer'),
+    cp              = require('child_process');
+
+
+/**
+* Vars
+*/
 
 var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+
 var messages = {
-    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+  jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
 };
+
+var paths = {
+  "sass": [
+    '_sass/*.scss'
+  ],
+
+  "js": ['.js/*.js']
+};
+
+
 
 /**
  * Build the Jekyll Site
  */
+
 gulp.task('jekyll-build', function (done) {
-    browserSync.notify(messages.jekyllBuild);
-    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
-        .on('close', done);
+  browserSync.notify(messages.jekyllBuild);
+  return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
+    .on('close', done);
 });
+
 
 /**
  * Rebuild Jekyll & do page reload
  */
+
 gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
-    browserSync.reload();
+  browserSync.reload();
 });
+
 
 /**
  * Wait for jekyll-build, then launch the Server
  */
+
 gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
-    browserSync({
-        server: {
-            baseDir: '_site'
-        }
-    });
+  browserSync({
+    server: {
+      baseDir: '_site'
+    }
+  });
 });
+
 
 /**
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
+
 gulp.task('sass', function () {
-    return gulp.src('_scss/main.scss')
-        .pipe(sass({
-            includePaths: ['scss'],
-            onError: browserSync.notify
-        }))
-        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('_site/css'))
-        .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('css'));
+  return gulp.src(paths.sass)
+    .pipe(sass({
+      includePaths: ['scss'],
+      onError: browserSync.notify
+    }))
+    .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+    .pipe(gulp.dest('_site/css'))
+    .pipe(browserSync.reload({stream:true}))
+    .pipe(gulp.dest('css'));
 });
+
 
 /**
  * Watch scss files for changes & recompile
  * Watch html/md files, run jekyll & reload BrowserSync
  */
+
 gulp.task('watch', function () {
-    gulp.watch('_scss/*.scss', ['sass']);
-    gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+  gulp.watch(paths.sass, ['sass'])
+    .on('change', function(event) {
+    var absPath = event.path;
+    var relPath = path.basename(absPath);
+    console.log(gutil.colors.yellow('CSS ') + relPath + ' was ' + gutil.colors.magenta(event.type) + ', running tasks...');
+  });
+  gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
+
 
 /**
  * Default task, running just `gulp` will compile the sass,
  * compile the jekyll site, launch BrowserSync & watch files.
  */
+
 gulp.task('default', ['browser-sync', 'watch']);
